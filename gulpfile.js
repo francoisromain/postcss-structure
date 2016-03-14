b57c13c9ab308f88475712a2022e8cf6cc8a5d69
Francois Romain <francoisromain@gmail.com>
2016-03-14 19:46:10 +0100

    Remove gruntfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,15 @@ var gulp = require('gulp');
 var postcss = require('gulp-postcss');
 var cssfmt = require('gulp-cssfmt');
 
-gulp.task('css', function() {
-  return gulp.src('./src/css/styles.css')
-    .pipe(postcss([
-      require('postcss-import'),
-      require('postcss-custom-properties'),
-      require('postcss-structure'),
-      require('postcss-cssnext')({browsers: "> 4%"}),
-      require('cssnano')({discardComments: {removeAll: true}})
-    ]))
-    .pipe(cssfmt())
-    .pipe(gulp.dest('./dist/css/'));
+gulp.task('css', function () {
+    return gulp.src('./src/css/styles.css')
+        .pipe(postcss([
+            require('postcss-import'),
+            require('postcss-custom-properties'),
+            require('postcss-structure'),
+            require('postcss-cssnext')({ browsers: "> 2%" }),
+            require('cssnano')({ discardComments: { removeAll: true }, autoprefixer: false })
+        ]))
+        .pipe(cssfmt())
+        .pipe(gulp.dest('./dist/css/'));
 });
