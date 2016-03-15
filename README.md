3c450ae4dfff9c482a07aa7bf295bdc261707e4b
Francois Romain <francoisromain@gmail.com>
2016-03-15 13:02:19 +0100

    Setup gh-pages

--- README.md
+++ README.md
@@ -1,12 +1,3 @@
 #structure.css
 
-todo
-- [x] remove compass 
-- [x] replace grunt with gulp
-- [x] replace sass / compass with postcss / cssnext
-- [x] write postcss plugin for grid: postcss-structure
-- [x] create variable page
-- [ ] publish on npm
-- [ ] em vs rem
-- [ ] media object
-- [ ] code styles
+gh-pages branch for https://github.com/francoisromain/structure.css
--- gulpfile.js
+++ gulpfile.js
@@ -4,21 +4,21 @@ var cssfmt = require('gulp-cssfmt');
 var livereload = require('gulp-livereload');
 
 gulp.task('css', function () {
-    return gulp.src('./styles.css')
+    return gulp.src('./src/css/styles.css')
         .pipe(postcss([
             require('postcss-import'),
             require('postcss-custom-properties'),
-            require('postcss-structure'),
+            require('./postcss-structure'),
             require('postcss-cssnext')({ browsers: "> 2%" }),
             require('cssnano')({ discardComments: { removeAll: true }, autoprefixer: false })
         ]))
         .pipe(cssfmt())
-        .pipe(gulp.dest('./dist/'))
+        .pipe(gulp.dest('./dist/css/'))
         .pipe(livereload());
 });
 
 // Rerun the task when a file changes
 gulp.task('watch', function () {
     livereload.listen();
-    gulp.watch(['css/**/*.css', 'styles.css'], ['css']);
+    gulp.watch(['src/css/**/*.css', 'postcss-structure/index.js'], ['css']);
 });
