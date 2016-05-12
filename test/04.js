b93d9eae2d41a8c59b4a662560dd091defe14298
Francois Romain <francoisromain@gmail.com>
2016-05-12 23:04:40 +0200

    fix test

--- test/04.js
+++ test/04.js
@@ -1,11 +1,21 @@
 const tests = {
-  input: `.my-button {
-  button: default;
+  input: `@button {
+  border-width: 1px;
+  color: orangered;
+  background-color: white;
+  border-color: silver;
+  color-hover: whitesmoke;
+  background-color-hover: orangered;
+  border-color-hover: orangered;
+  color-active: whitesmoke;
+  background-color-active: silver;
+  border-color-active: silver;
+  class-active: active;
+  class-disabled: disabled;
 }
 
-.my-button-2 {
-  button-color: red pink blue green;
-  button-border: 2px black red;
+.my-button {
+  button: test;
 }
 `,
   output: `.my-button, .my-button:visited {
@@ -14,40 +24,26 @@ const tests = {
   cursor: pointer;
   text-decoration: none;
   border: none;
-  color: grey;
+  color: orangered;
   background-color: white;
+  box-shadow: inset 0 0 0 1px silver;
 }
-
-.my-button:active, .my-button:hover, .my-button.active {
-  color: white;
-  background-color: grey;
+.my-button:active, .my-button.active {
+  color: whitesmoke;
+  background-color: silver;
+  box-shadow: inset 0 0 0 1px silver;
 }
-
+.my-button:hover, .my-button.active:hover {
+  color: whitesmoke;
+  background-color: orangered;
+  box-shadow: inset 0 0 0 1px orangered;
+}
 .my-button:disabled, .my-button:disabled:active, .my-button:disabled:hover, .my-button.disabled, .my-button.disabled:active, .my-button.disabled:hover {
   opacity: 0.25;
   cursor: default;
-}
-
-.my-button-2, .my-button-2:visited {
-  display: inline-block;
-  margin: 0;
-  cursor: pointer;
-  text-decoration: none;
-  border: none;
-  color: red;
-  background-color: pink;
-  box-shadow: inset 0 0 0 2px black;
-}
-
-.my-button-2:active, .my-button-2:hover, .my-button-2.active {
-  color: blue;
-  background-color: green;
-  box-shadow: inset 0 0 0 2px red;
-}
-
-.my-button-2:disabled, .my-button-2:disabled:active, .my-button-2:disabled:hover, .my-button-2.disabled, .my-button-2.disabled:active, .my-button-2.disabled:hover {
-  opacity: 0.25;
-  cursor: default;
+  color: orangered;
+  background-color: white;
+  box-shadow: inset 0 0 0 1px silver;
 }
 `,
 };
